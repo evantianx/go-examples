447be9c9ce69abe3426efdaf0e58d748513ee53b
evantianx <evantianx@gmail.com>
2020-10-12 11:00:29 +0800

    feat: loop

--- loop.go
+++ loop.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -14,6 +16,28 @@ func convertToBin(n int) string {
 	return res
 }
 
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func forever() {
+	for {
+		fmt.Println("abc")
+	}
+}
+
 func main()  {
 	fmt.Println(convertToBin(5), convertToBin(13))
+
+	printFile("abc.txt")
+
+	forever()
 }
